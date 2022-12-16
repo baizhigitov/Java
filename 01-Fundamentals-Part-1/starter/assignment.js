e69539b2f2e1c9491a9d21d7a0d11da0a1882614
Islambek Baizhigitov <islambekbayjigitov@gmail.com>
2022-12-16 14:37:58 +0100

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,10 @@
+let myRole = "Salesforce Developer";
+console.log(myRole);
+
+// Lecture: Values and Variables
+let country = 'Kyrgyzstan';
+let continent = 'Central Asia';
+let population = 7000000;
+console.log(country);
+console.log(continent);
+console.log(population);
