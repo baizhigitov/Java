71e8d00ddf23d1f0e6f23fecd856b3b240c49652
Islambek Baizhigitov <islambekbayjigitov@gmail.com>
2022-12-27 12:57:55 +0100

    s

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -91,13 +91,14 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100){
 
 */
 
+/*
 // Coding challenge #4
 
 const bill = 430;
 const tip = bill >= 50 &&  bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 console.log(`The bill was ${bill}$, the tip was ${tip}$, and the total value ${bill + tip}$`);
-
+*/
 
 
 
