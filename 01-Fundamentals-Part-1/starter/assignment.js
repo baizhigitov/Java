42b5db131d47d4f2f919c58859d9d61c968e4108
Islambek Baizhigitov <islambekbayjigitov@gmail.com>
2022-12-27 12:55:47 +0100

    all assignments from Fundamentals 1

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,10 +1,220 @@
+/*
 let myRole = "Salesforce Developer";
 console.log(myRole);
+// Data types
+let goingToParty = true;
+console.log(goingToParty);
+console.log(typeof true);
+goingToParty = 'Yes';
+console.log(typeof goingToParty);
 
-// Lecture: Values and Variables
-let country = 'Kyrgyzstan';
-let continent = 'Central Asia';
-let population = 7000000;
-console.log(country);
-console.log(continent);
-console.log(population);
+let year = 1997;
+console.log(year);
+
+// Let: Value can be changed,  Const: Value never changing,  Var
+
+const birthYear = 1997;
+console.log(birthYear);
+
+// Operators
+const now = 2022;
+const ageIslambek = now - 1997; 
+const ageAizirek = now - 2003;
+console.log(ageIslambek, ageAizirek);
+console.log(ageIslambek - ageAizirek);
+console.log(ageIslambek*2 , ageAizirek/2);
+
+const name = 'Islambek';
+const lastName = 'Baizhigitov';
+console.log(name + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10;  // x = x + 10 = 25
+x *= 4; //  x = x * 4 = 100
+x++; // x = x + 1
+x--;
+console.log(x);
+
+// Comparison operators 
+console.log(ageAizirek < ageIslambek);
+
+const isFullAge = ageAizirek >= 18;
+
+let f, y;
+f= y = 25-10-5; // f = y = 10, f = 10
+console.log(f,y);
+
+const averageAge = (ageAizirek + ageIslambek) / 2;
+console.log (ageAizirek, ageIslambek , averageAge);
+*/
+
+////////////////////////////////////
+// Coding Challenge #1,2 
+
+/*
+let namePersonOne = 'Mark';
+let namePersonTwo = `John`;
+let weightOfMark = 95;
+let heightOfMark = 1.88;
+let BMIOfMark = weightOfMark / heightOfMark ** 2 ;
+console.log(BMIOfMark);
+
+let weightOfJohn = 85;
+let heightOfJohn = 1.76;
+let BMIOfJohn = weightOfJohn / heightOfJohn ** 2;
+console.log(BMIOfJohn);
+
+let highestBMI;
+if (BMIOfJohn < BMIOfMark){
+    highestBMI = `${namePersonOne}'s BMI (${BMIOfMark}) is higher than ${namePersonTwo}`;
+} else {
+    highestBMI = `${namePersonTwo}'s BMI (${BMIOfJohn}) is higher than ${namePersonOne}`;
+}
+console.log(highestBMI);
+*/
+
+/*
+// Coding Challenge #3
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100){
+    console.log('Dolphins is the winner!');
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100){
+    console.log('Koalas is the winner!');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100 ){
+    console.log('The game is DRAW');
+} else {
+    console.log('No one wins the trophy (')
+}
+
+*/
+
+// Coding challenge #4
+
+const bill = 430;
+const tip = bill >= 50 &&  bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}$, the tip was ${tip}$, and the total value ${bill + tip}$`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const firstName = 'Aizirek';
+const job = 'mobilograph';
+const birthYear = 2003;
+const year = 2023;
+// using backticks template literals
+const aizi = `I'm ${firstName}, a ${year - birthYear} year old ${job}!;`
+console.log(aizi) ;
+console.log(`Yuou
+sdfsd
+sdf`); // multiple line template literals
+*/
+
+/*
+const age = 19;
+if (age >= 18){
+    console.log('Aizi can start driving license)');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Aizi is too young. Wait another ${yearsLeft};)`);
+}
+
+*/
+
+/*
+const birthYear = 2003;
+
+let century;
+if (birthYear <= 2000){
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);   */
+
+
+/*
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+// type coertion
+
+const testingTypeCoertion = 5+6+'4'+9-4-2;
+console.log(testingTypeCoertion);
+
+const age = 18;
+if (age === 18) console.log('You just became an adult ');
+
+
+const favourite = prompt ('What is your favourite number?');
+console.log(Number(favourite));
+*/
+
+
+/*
+const hasDriversLicense = true; //A
+const hasGoodVision = true; //B
+const isTired = false; //C 
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+if (hasDriversLicense && hasGoodVision && !isTired){
+    console.log('Aizi is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
+*/
+
+
+/*
+const day = 'tuesday';
+
+switch (day){
+    case 'monday': //day === 'monday'
+     console.log('Plan course structure');
+     console.log('Go to coding meetup');
+    break;
+    case 'tuesday':
+     console.log('Prepare theory videos');
+    break;
+    case 'wednesday':
+    case 'thursday':
+     console.log('Write code examples');
+    break;
+    case 'friday':
+     console.log('Record videos');
+    break;
+    case 'saturday':
+    case 'sunday':
+     console.log('Enjoy the weeekend ;D');
+    break;
+    default:
+     console.log('Not a valid day!');
+
+}
+
+*/
+
+/*
+const age = 25;
+const drink = age >= 18 ? ' can drink energetic drink' : 'water';
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? 'energetic drink' : 'water'}`);
+
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,133 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40+8+23-10);
 console.log('Jonas');
 console.log(23);
 let firstName = 'Islambek';
 console.log(firstName);
+*/
+
+/*
+
+// Assignments !!!
+// Lecture: Values and Variables
+let country = 'Kyrgyzstan';
+let continent = 'Central Asia';
+let population = 7000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(`${country}'s population is ${population > 33000000 ? 'above' : 'below'} average`);
+
+*/
+/*
+// Lecture: Data types
+let isIsland = 'Central Asia';
+let language;
+console.log (isIsland, population, country, language);
+
+// Lecture: let , const , var
+language = "kyrgyz language";
+console.log(isIsland, population, country, language);
+
+const myResult = (isIsland + ' ' + population + ' ' + country + ' ' + language);
+console.log(myResult);
+
+
+// Lecture: Basic operators
+let dividedPopulation;
+dividedPopulation = population /2 ;
+console.log(dividedPopulation);
+dividedPopulation ++;
+console.log(dividedPopulation);
+
+let myFriendsCountry = 'Finland';
+let populationOfFinland = 6000000;
+let comparisonOfCountries;
+console.log(dividedPopulation >= myFriendsCountry);
+comparisonOfCountries = (dividedPopulation >= myFriendsCountry);
+let averagePopulation;
+averagePopulation = 33000000;
+comparisonOfCountries = (dividedPopulation >= averagePopulation);
+
+// Taking Decisions: if / else Statements
+let checkAverage;
+if (population >= averagePopulation){
+    checkAverage = `${country}'s population is above average`;
+} else {
+    checkAverage = `${country}'s population is below average`;
+}
+console.log(checkAverage);
+console.log(comparisonOfCountries);
+
+console.log(country + ' is in ' + isIsland + ' , and its ' + population + ' people speak ' + language);
+
+let description = (`${country} is in ${isIsland}, and its ${population} people speak ${language}`);
+
+console.log(description);
+*/
+
+/*
+// Lecture; Equality Operators == vs ===
+const numNeighbours = prompt ('How many neighbour countries does your country have?'); 
+const answerPromt = numNeighbours;
+console.log(Number (answerPromt));
+if (numNeighbours === 1){
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1 ){
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
+*/
+
+/*
+// Logical Operators
+const countryToLive = 'Portugal';
+const spokenEnglish = true; 
+const numberOfPeople = 35000000;
+const notIsland = true; 
+if (spokenEnglish === true && numberOfPeople < 50000000 && notIsland === true){
+    console.log(`You should live in ${countryToLive}`);
+} else {
+    console.log(`${countryToLive} does not meet your criteria`);
+}
+*/
+
+
+/*
+// the switch statement
+const language = 'arabic';
+
+switch (language){
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+    break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+    break;
+    case 'english':
+        console.log('3rd place');
+    break;
+    case 'hindi':
+        console.log('Number 4');
+    break;
+    case 'arabic':
+        console.log('5th most spoken language');
+    break;
+    default:
+        console.log('Great language too :D');
+
+}
+
+*/
+
+
+/*
+// The conditional (ternary) operator
+const countryName = 'Kyrgyzstan';
+const numberPopulation = 33000000;
+const comparisonOfPopulation = numberPopulation >= 33000000 ? `${countryName}'s population is above average` : `${countryName}'s population is below average`;
+console.log(comparisonOfPopulation);
+
+*/
